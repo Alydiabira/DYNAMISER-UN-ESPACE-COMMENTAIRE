839b4a441358f41b61c341d32fc2d7ede1f46834
Aly <d-aly@outlook.fr>
2024-03-06 17:25:25 +0100

    Update alert.js

--- function/alert.js
+++ function/alert.js
@@ -1,8 +1,15 @@
 /**
  * Renvoi un élément HTML représentant une alert 
  * @param {*} message 
+ * @param {HTMLElement} 
  */
 export function alertElemnt( message) { 
     const el = document.querySelector('#alert').content.cloneNode(true)
     el.querySelector('.js-text').innerHTML = message
+    el.querySelector('button').addEvenListener('click', e => {
+        e.preventDefault()
+        el.remove()
+    }) = message
+return el
+
 }
