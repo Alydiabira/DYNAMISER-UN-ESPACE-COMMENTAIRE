2e268dbb010b05b64e68460b024ca8fe1706629c
Aly <d-aly@outlook.fr>
2024-03-06 17:55:05 +0100

    Update alert.js

--- function/alert.js
+++ function/alert.js
@@ -3,13 +3,16 @@
  * @param {*} message 
  * @param {HTMLElement} 
  */
-export function alertElemnt( message) { 
-    const el = document.querySelector('#alert').content.cloneNode(true)
+export function alertElement( message) { 
+    /** @type {HTMLElement} */
+    const el = document.querySelector('#alert').content.firstElement.cloneNode(true)
     el.querySelector('.js-text').innerHTML = message
     el.querySelector('button').addEvenListener('click', e => {
         e.preventDefault()
+        console.log(el)
         el.remove()
-    }) = message
-return el
+        el.dispatchEvent(new CustomEvent('close'))
+    }) 
+    return el
 
 }
